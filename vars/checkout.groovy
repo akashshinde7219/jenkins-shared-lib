985ef505a867b005e77dad07ea2e71d53fe45bc2
Akash Shinde <Firstname.lastname@realthingks.com>
2025-04-17 12:18:36 +0530

    chechout function added

---
+++ vars/checkout.groovy
@@ -0,0 +1,12 @@
+def chechout(string giturl, string branch='main'){
+    echo "Checking out branch: ${branch}"
+
+    chechout([
+        $class :'GitSCM'
+        branches: [[name:branch]],
+        userRemoteConfigs:[[url:giturl]]
+    ])
+
+
+
+}
