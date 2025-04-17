10f30c12b7bb4ee2dae610809227622169133515
Akash Shinde <Firstname.lastname@realthingks.com>
2025-04-17 12:21:54 +0530

    chechout function added

--- vars/clonerepo.groovy
+++ vars/clonerepo.groovy
@@ -2,7 +2,7 @@ def chechout(string giturl, string branch='main'){
     echo "Checking out branch: ${branch}"
 
     chechout([
-        $class :'GitSCM'
+        $class :'GitSCM',
         branches: [[name:branch]],
         userRemoteConfigs:[[url:giturl]]
     ])
